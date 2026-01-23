7469ad8d5fe4ad115c00648b7a8adce11c3ebc2d
coderheck <loxzyioxzybusiness@hotmail.com>
2026-01-23 21:12:13 +0700

    updated discord.bat

--- discord.bat
+++ discord.bat
@@ -2,4 +2,20 @@
 @echo off
 setlocal EnableDelayedExpansion
 set LookDir=%localappdata%\Discord
-for /d %%d in (%LookDir%\*) do pushd %%d & (for %%z in (*.exe) do start %%z) & popd
+for /d %%d in (%LookDir%\*) do (
+	for %%z in ("%%d\*.exe") do (
+		REM echo %%d %%z
+		if /I %%~nxz==Discord.exe (
+			REM echo %%d %%z 
+			echo [found executable: %%d\%%z]
+			cd %%d && start %%z
+			exit /b
+		)
+	)
+)
+REM where /r %LookDir% "discord.exe" | clip
+REM if errorlevel 1 (
+REM 	echo [code 1: discord executable not found]
+REM ) else (
+REM 	echo [code 0: found discord executable]
+REM )
