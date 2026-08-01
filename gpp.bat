f1dd6d2aa5cf2df53687f5e65c55ef2bc2e47e14
coderheck <loxzyioxzybusiness@hotmail.com>
2026-08-01 21:56:33 +0700

    gpp: fix for paths containing spaces

--- gpp.bat
+++ gpp.bat
@@ -6,7 +6,7 @@ goto compile
 
 :compile
 cd "%~dp1"
-g++ -std=c++23 -Wall -Wextra -Wpedantic -Werror -Winvalid-pch -DONLINE_JUDGE -O2 -lm -fmax-errors=5 -march=native -Wl,--stack,16777216 -s -o "%~dpn1" "%1" -lstdc++exp
+g++ -std=c++23 -Wall -Wextra -Wpedantic -Werror -Winvalid-pch -DONLINE_JUDGE -O2 -lm -fmax-errors=5 -march=native -Wl,--stack,16777216 -s -o "%~dpn1" %1 -lstdc++exp
 call progtime "%~dpn1.exe"
 del "%~dpn1.exe"
 
